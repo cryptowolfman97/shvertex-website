3bb85378805907b63bf3f1379ba1b74e760da85d
cryptowolfman97 <cryptowolfman97@gmail.com>
2026-05-25 14:16:57 +0530

    Delete shv-supabase-config.js

--- shv-supabase-config.js
+++
@@ -1,9 +0,0 @@
-window.SHV_SUPABASE = {
-  url: 'https://ovdxetyadfsxehwnbyuz.supabase.co',
-  publishableKey: 'sb_publishable_3J-H60daCgWdhSvpdXi0zw_QpPax3Dz',
-
-  siteUrl: 'https://shvertex.online',
-  storageBucket: 'app-backups',
-  defaultAppCode: 'simplibudget',
-  defaultPlan: 'Standard'
-};
