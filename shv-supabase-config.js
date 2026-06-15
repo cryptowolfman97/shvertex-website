3c5c696ec7336472c1e73ba5c48837359b738446
cryptowolfman97 <cryptowolfman97@gmail.com>
2026-06-15 12:11:40 +0530

    Upload shv-supabase-config.js

---
+++ shv-supabase-config.js
@@ -0,0 +1,10 @@
+window.SHV_SUPABASE = {
+  url: 'https://ovdxetyadfsxehwnbyuz.supabase.co',
+  publishableKey: 'sb_publishable_3J-H60daCgWdhSvpdXi0zw_QpPax3Dz',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92ZHhldHlhZGZzeGVod25ieXV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyOTg3ODUsImV4cCI6MjA5MTg3NDc4NX0.LBPevOt31jpJaQNK7n_5GQsD-H40ndFVmi7dNeJt5tA',
+
+  siteUrl: 'https://shvertex.online',
+  storageBucket: 'app-backups',
+  defaultAppCode: 'simplibudget',
+  defaultPlan: 'Standard'
+};
